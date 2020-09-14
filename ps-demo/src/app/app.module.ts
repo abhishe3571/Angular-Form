d90b125c12c74d80c955f7621e7418d46cb01716
Abhishek Singh <abhishek3571@gmail.com>
2020-09-15 04:18:53 +0530

    Completed Form Basics in Angular Form from Mark Zamoyta pluralsight tutorials

--- ps-demo/src/app/app.module.ts
+++ ps-demo/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, UserSettingsFormComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
