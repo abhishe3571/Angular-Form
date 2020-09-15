80402ff075f63bbb8fbd791730e48a5511a4b7f2
Abhishek Singh <abhishek3571@gmail.com>
2020-09-15 09:42:52 +0530

    Completed Third-party Form Controls from Mark Zamoyta angular form tutorials

--- ps-demo/src/app/app.module.ts
+++ ps-demo/src/app/app.module.ts
@@ -3,12 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+import { RatingModule } from 'ngx-bootstrap/rating';
+
+
 import { AppComponent } from './app.component';
 import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
 
 @NgModule({
   declarations: [AppComponent, UserSettingsFormComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    TimepickerModule.forRoot(),
+    RatingModule.forRoot(),
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -18,6 +18,15 @@ export class UserSettingsFormComponent implements OnInit {
     subscriptionType: 'Annual',
     notes: 'Here are some notes',
   };
+
+  // tslint:disable-next-line: quotemark
+  singleModel = "On";
+
+
+  startDate: Date;
+  startTime: Date;
+  userRating: 0;
+  maxRating: 10;
   userSettings: UserSettings = { ...this.originalUserSettings };
   postError = false;
   postErrorMessage = '';
@@ -27,6 +36,9 @@ export class UserSettingsFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.subscriptionTypes = this.dataService.getSubscriptionTypes();
+
+    this.startDate = new Date();
+    this.startTime = new Date();
   }
 
   onBlur(field: NgModel) {
@@ -40,15 +52,15 @@ export class UserSettingsFormComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log('in onSubmit: ', form.valid);
-    if (form.valid) {
-      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
-        (result) => console.log('success: ', result),
-        (error) => this.onHttpError(error)
-      );
-    } else {
-      this.postError = true;
-      this.postErrorMessage = 'Plz fix the above errors';
-    }
+    console.log('in onSubmit: ', form.value);
+    // if (form.valid) {
+    //   this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+    //     (result) => console.log('success: ', result),
+    //     (error) => this.onHttpError(error)
+    //   );
+    // } else {
+    //   this.postError = true;
+    //   this.postErrorMessage = 'Plz fix the above errors';
+    // }
   }
 }
